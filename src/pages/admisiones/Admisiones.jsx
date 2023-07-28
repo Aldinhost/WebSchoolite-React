5f87389eb8bf0262742c19330311cfa17dfe1158
Aldinhost <mardelaldo@gmail.com>
2023-07-28 13:58:25 -0600

    Check error admisiones4

--- src/pages/admisiones/Admisiones.jsx
+++ src/pages/admisiones/Admisiones.jsx
@@ -3,7 +3,6 @@ import Footer from "../../components/footer/Footer";
 import NavbarFix from "../../components/navbar/Navbar";
 import Requisitos from "../../components/requisitos/Requisitos";
 import FormGeneral from "../../components/formGral/FormGral";
-import '/admisiones.scss';
 
 const Admisiones = () => {
   const [preescolar, setPreescolar] = useState(false);
